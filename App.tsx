9ab7bcef5d146119387212778011ff8d162cfd47
Soomin Choi <soomin.choi@fphcare.co.nz>
2024-02-05 16:02:07 +1300

    add image background with punch hole PNG - TODO: add video tile

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ReactNode } from "react";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const image = require("./assets/punch_hole_view.png");
+const GAP = 20;
+const IMAGE_WIDTH = Dimensions.get("screen").width - GAP;
+const IMAGE_HEIGHT = Dimensions.get("screen").height;
+interface PunchHoleViewProps {
+  children: ReactNode;
+}
+const PunchHoleView: React.FC<PunchHoleViewProps> = ({ children }) => {
+  return (
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      {children}
+    </ImageBackground>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <PunchHoleView>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </PunchHoleView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    // self
+    flex: 1,
+    width: IMAGE_WIDTH,
+    height: IMAGE_HEIGHT,
+    alignSelf: "flex-start",
+    // shadow
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, // For Android
+
+    // child
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
